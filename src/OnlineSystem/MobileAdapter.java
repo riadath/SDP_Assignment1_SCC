e0871e44f78c9dca9791457b6ffa3d83cbaccc1d
riadath <akib152012@gmail.com>
2023-08-23 00:51:20 +0600

    online system, mobile + webadapter

---
+++ src/OnlineSystem/MobileAdapter.java
@@ -0,0 +1,9 @@
+package OnlineSystem;
+
+import java.time.LocalDate;
+
+public interface MobileAdapter {
+    void requestCarServicing(String name, LocalDate date);
+    void requestCarWash(String name, LocalDate date);
+    void requestOnlineDelivery(String name, LocalDate date);
+}
---
+++ src/OnlineSystem/WebAdapter.java
@@ -0,0 +1,24 @@
+package OnlineSystem;
+
+import java.time.LocalDate;
+
+public class WebAdapter implements MobileAdapter{
+    private CentralOnlineSystem centralOnlineSystem;
+    @Override
+    public void requestCarServicing(String name, LocalDate date) {
+        Command command = new RequestCarServicingCommand(name, date);
+        centralOnlineSystem.addCommand(command);
+    }
+
+    @Override
+    public void requestCarWash(String name, LocalDate date) {
+        Command command = new RequestCarWashCommand(name, date);
+        centralOnlineSystem.addCommand(command);
+    }
+
+    @Override
+    public void requestOnlineDelivery(String name, LocalDate date) {
+        Command command = new RequestOnlineDeliveryCommand(name, date);
+        centralOnlineSystem.addCommand(command);
+    }
+}
